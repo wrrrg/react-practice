ed3390a70f46c707e2898c9a3f226e0aa5969541
wrrrg <bill.gryta@gmail.com>
2018-05-17 03:16:08 -0500

    I'm so dumb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = PROCESS.ENV.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send({ hi: 'hello there' });
