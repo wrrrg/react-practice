f516c17cb22a55f9632245c4d5366570e0545458
wrrrg <bill.gryta@gmail.com>
2018-05-17 08:17:34 -0500

    basic structure google oauth passport

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+// Mongo Nonsense
+const mongoose = require('mongoose');
+const { mongoURI, cookieKey } = require('./config/keys');
+
+mongoose.connect(mongoURI, err => {
+  if (err) {
+    throw err;
+  }
+  console.log('Successfully connected to mongo!');
+});
+require('./models/User');
+
+//Passport has to be after Mongo or it breaks
 require('./services/passport');
 
 const app = express();
 
+//this tells express to use cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
+);
+
+// this tells passport to use cookies
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const { googleClientID, googleClientSecret } = require('./../config/keys');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(`access token: ${accessToken}`);
-      console.log(`refresh token: `, refreshToken);
-      console.log(`profile: `, profile.id);
-      done();
+      // console.log(`access token: ${accessToken}`);
+      // console.log(`refresh token: `, refreshToken);
+      // console.log(`profile: `, profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          var newUser = new User({ googleId: profile.id });
+          newUser.save().then(user => done(null, user));
+        }
+      });
     }
   )
 );
